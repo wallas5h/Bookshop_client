71897d41e8d314b2f99767e1377371ffdf87d48f
wallas5h@gmail.com <wallas5h@gmail.com>
2022-08-21 12:16:41 +0200

    add cartId in cartSlice

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -2,25 +2,39 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface CartChangeStateInterface {
   changeBetweenCartWishlist: number;
+  cartId: string | null;
 }
 
-const initialState = {
+const initialState: CartChangeStateInterface = {
   changeBetweenCartWishlist: 0,
-}
+  cartId: null,
+};
 
 interface AddChangeBetweenCartWishlist {
-  payload: number
+  payload: number;
 }
 
+interface SetCartId {
+  payload: string | null;
+}
 
 export const cartWishlistSlice = createSlice({
-  name: 'cartWishlist',
+  name: "cartWishlist",
   initialState,
   reducers: {
-    addChangeBetweenCartWishlist: (state, action: AddChangeBetweenCartWishlist) => {
-      state.changeBetweenCartWishlist = action.payload
+    addChangeBetweenCartWishlist: (
+      state,
+      action: AddChangeBetweenCartWishlist
+    ) => {
+      state.changeBetweenCartWishlist = action.payload;
     },
-  }
-})
+    setCartId: (state, action: SetCartId) => {
+      state.cartId = action.payload;
+    },
+  },
+});
 
-export const { addChangeBetweenCartWishlist } = cartWishlistSlice.actions
+export const {
+  addChangeBetweenCartWishlist,
+  setCartId,
+} = cartWishlistSlice.actions;
